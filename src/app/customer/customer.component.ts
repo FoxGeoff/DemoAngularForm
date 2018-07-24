70ff691f89aef6cdc70bf495715b6e3bd84a4421
FoxGeoff <fox.geoffrey@gmail.com>
2018-07-24 16:58:04 -0700

    Add Customer Form

---
+++ src/app/customer/customer.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Customer } from './Customer';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css']
+})
+export class CustomerComponent implements OnInit {
+
+  customer: Customer = new Customer();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  save(customerForm: NgForm) {
+    console.log(customerForm.form);
+    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  }
+
+}
+
+
